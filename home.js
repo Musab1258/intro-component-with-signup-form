1dd41f06d5ee1eb38a35c5763af45daea6b363bd
Musab Habeeb <habeebmusab@gmail.com>
2021-10-07 16:00:03 +0100

    first commit

---
+++ home.js
@@ -0,0 +1,15 @@
+function alterPage() {
+   document.getElementsByTagName('form').addEventListener('submit', alert('wow'))
+}
+
+
+/*function validate() {
+  document.getElementsByTagName('form').addEventListener('submit' validateForm())
+}
+
+const validateForm = function validateForm1() {
+  const falt = document.getElementByClassName(form-input).innerHTML;
+  if (falt == "") {
+    alert('no');
+  }
+}
